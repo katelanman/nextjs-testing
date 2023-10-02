eb9591d447c70006f5b616b7edf30df112ebe4a6
Kate Lanman <katelanman@Kates-MacBook-Pro.local>
2023-10-02 08:51:41 -0400

    temporarily removing d3 usage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Layout from '../components/layout';
 import React, { useState, useEffect } from "react";
-import BarChart from './test-bar';
+// import BarChart from '../test-bar';
 import Map from './map3.js';
 import MeshMap from './mesh-map.js';
 import useMediaQuery from '@mui/material/useMediaQuery';
@@ -32,7 +32,7 @@ function VisContents({topic, data, resolution, region, year}) {
         <SummaryItem value="0.82%" description="average percentage of income for internet"></SummaryItem>
         <SummaryItem value="0.74%" description="median percentage of income for internet"></SummaryItem>
       </div>
-      <BarChart data={data} />
+      {/* <BarChart data={data} /> */}
     </div>
   );
 };
